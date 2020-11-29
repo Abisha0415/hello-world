eabb267458eab21f90b47ab9b7f9e49da62b10ef
Abisha0415 <74620510+Abisha0415@users.noreply.github.com>
2020-11-28 21:35:26 -0600

    Create coding_practice_2020_11_27.py

---
+++ coding_practice_2020_11_27.py
@@ -0,0 +1,70 @@
+
+print("Excercise 1")
+# from 385 to 493 by 9 
+i = 385 
+while i <= 493: 
+    print(i)
+    i += 9 
+
+print("Excercise 2")
+# from 429 to 625 by 7
+i = 429
+while i <= 625: 
+    print(i)
+    i += 7
+
+print("Excercise 3")
+# from -11 to 781 by 18 
+i = -11
+while i <= 781: 
+    print(i)
+    i += 18
+
+print("Excercise 4")
+# from 54 to 186 by 6
+i = 54
+while i <= 186: 
+    print(i)
+    i += 6
+
+print("Excercise 5")
+# from 280 to 392 by 8 
+i = 280
+while i <= 392: 
+    print(i)
+    i += 8
+
+print("Excercise 6")
+# from -373 to -281 by 2
+i = -373
+while i <= -281: 
+    print(i)
+    i += 2
+
+print("Excercise 7")
+# from 392 to 843 by 11
+i = 392
+while i <= 843: 
+    print(i)
+    i += 11
+
+print("Excercise 8")
+# from 15 to 429 by 9 
+i = 15 
+while i <= 429: 
+    print(i)
+    i += 9 
+
+print("Excercise 9")
+# from 113 to 181 by 2 
+i = 113
+while i <= 181: 
+    print(i)
+    i += 2
+
+print("Excercise 10")
+# from -421 to =197 by 8 
+i = -421
+while i <= 197: 
+    print(i)
+    i += 8  
